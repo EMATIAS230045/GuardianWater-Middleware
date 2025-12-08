7e5597b77d2fb5aab541a7f3f6ee44c1a1de979d
Erick Matias Granillo Mejia <230045@utxicotepec.edu.mx>
2025-12-08 16:08:10 -0600

    Actualizacion del componente login
    Actualizacion del componete para que pueda enviar y recibir peticiones y respuestas del backEnd ademas de integrar funcionalidad con el middleware

--- GuardianWater-Frontend/src/app/components/login/login.ts
+++ GuardianWater-Frontend/src/app/components/login/login.ts
@@ -1,15 +1,29 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service'; 
+import { CommonModule } from '@angular/common'; 
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
 	selector: 'app-login-container',
-	imports: [],
+	standalone: true,
+    imports: [
+        CommonModule,
+        FormsModule 
+    ],
 	templateUrl: './login.html',
 	styleUrl: './login.css',
 })
 
 export class LoginComponent {
-	constructor(private router: Router) {}
+
+    email: string = "";
+    password: string = "";
+    errorMessage: string = ""; 
+	constructor(private router: Router,
+		private authService: AuthService 
+	) {}
+
 	@Output() loginSuccess = new EventEmitter<void>();
 	login: boolean = true;
 
@@ -18,8 +32,20 @@ export class LoginComponent {
 	}
 
 	push_login() {
-		this.loginSuccess.emit(); //notifica al componente principal
-		this.router.navigate(['/inicio']);
+		this.errorMessage = "";
+        
+       
+        this.authService.login(this.email, this.password)
+            .subscribe({
+                next: (res) => {
+                    console.log("Login exitoso", res);
+                    this.loginSuccess.emit(); 
+                    this.router.navigate(['/inicio']); 
+                },
+                error: (err) => {
+                    this.errorMessage = err.error.error || "Error de conexión o credenciales.";
+                }
+            });
 	}
 	push_registro() {
 		this.login = true;
