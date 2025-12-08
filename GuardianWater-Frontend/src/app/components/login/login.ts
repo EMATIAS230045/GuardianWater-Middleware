89a05f9d2b19610ed2d4517a4a966c8f19e62f1a
Erick Matias Granillo Mejia <230045@utxicotepec.edu.mx>
2025-12-07 21:13:09 -0600

    Error con componente login
    Se esta haciendo el arreglo de un error de componetes

--- GuardianWater-Frontend/src/app/components/login/login.ts
+++ GuardianWater-Frontend/src/app/components/login/login.ts
@@ -4,29 +4,39 @@ import { AuthService } from '../../services/auth.service';
 
 @Component({
 	selector: 'app-login-container',
+	standalone: true,
 	imports: [],
 	templateUrl: './login.html',
 	styleUrl: './login.css',
 })
 
 export class LoginComponent {
-	constructor(private router: Router,
+	constructor(
+	private router: Router,
 	private authService: AuthService
 	) {}
-	email: string = "";
-  	password: string = "";
-  	errorMessage: string = "";
-
 	@Output() loginSuccess = new EventEmitter<void>();
 	login: boolean = true;
 
+	email: string = "";
+  	password: string = "";
+  	errorMessage: string = "";
+	
 	registrarse() {
 		this.login = !this.login;
 	}
 
 	push_login() {
-		this.loginSuccess.emit(); //notifica al componente principal
-		this.router.navigate(['/inicio']);
+		this.authService.login(this.email, this.password)
+		.subscribe({
+        next: (res) => {
+          console.log("Login exitoso", res);
+          this.router.navigate(['/inicio']);
+        },
+        error: (err) => {
+          this.errorMessage = err.error.error || "Error en el login";
+        }
+      });
 	}
 	push_registro() {
 		this.login = true;
