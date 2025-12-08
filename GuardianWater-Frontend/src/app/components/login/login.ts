3aa123b23bb11b2717c2dc249ee34bd6ac4ebaba
Erick Matias Granillo Mejia <230045@utxicotepec.edu.mx>
2025-12-08 15:50:22 -0600

    Inclusión global de HTTPClient
    Se añadio los la importacion de forma global para todos los componentes de la page de Angular

--- GuardianWater-Frontend/src/app/components/login/login.ts
+++ GuardianWater-Frontend/src/app/components/login/login.ts
@@ -1,42 +1,25 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from '../../services/auth.service';
 
 @Component({
 	selector: 'app-login-container',
-	standalone: true,
 	imports: [],
 	templateUrl: './login.html',
 	styleUrl: './login.css',
 })
 
 export class LoginComponent {
-	constructor(
-	private router: Router,
-	private authService: AuthService
-	) {}
+	constructor(private router: Router) {}
 	@Output() loginSuccess = new EventEmitter<void>();
 	login: boolean = true;
 
-	email: string = "";
-  	password: string = "";
-  	errorMessage: string = "";
-	
 	registrarse() {
 		this.login = !this.login;
 	}
 
 	push_login() {
-		this.authService.login(this.email, this.password)
-		.subscribe({
-        next: (res) => {
-          console.log("Login exitoso", res);
-          this.router.navigate(['/inicio']);
-        },
-        error: (err) => {
-          this.errorMessage = err.error.error || "Error en el login";
-        }
-      });
+		this.loginSuccess.emit(); //notifica al componente principal
+		this.router.navigate(['/inicio']);
 	}
 	push_registro() {
 		this.login = true;
