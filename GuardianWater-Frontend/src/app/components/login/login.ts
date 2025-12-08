b10358f83cfa7d860683cd1e97b63a84e3d8b7f7
Erick Matias Granillo Mejia <230045@utxicotepec.edu.mx>
2025-12-07 20:42:46 -0600

    Creación de nuevo servicio
    Creación de un servicio que nos ayudara a comunicar el apartado del backEnd con el frontEnd para el apartado del login

--- GuardianWater-Frontend/src/app/components/login/login.ts
+++ GuardianWater-Frontend/src/app/components/login/login.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
 	selector: 'app-login-container',
@@ -9,7 +10,13 @@ import { Router } from '@angular/router';
 })
 
 export class LoginComponent {
-	constructor(private router: Router) {}
+	constructor(private router: Router,
+	private authService: AuthService
+	) {}
+	email: string = "";
+  	password: string = "";
+  	errorMessage: string = "";
+
 	@Output() loginSuccess = new EventEmitter<void>();
 	login: boolean = true;
 
